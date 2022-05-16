02d30c1b430591e3c92044f497fe705c8811c939
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-05-16 20:55:53 +0300

    Add new repos

---
+++ C# Advanced/01.Lab Stacks and Queues/05. Print Even Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _05._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+
+            
+        }
+    }
+}
