f7866359e67e2d2a29ebe7f885e8f2f11034a9a3
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-05-16 20:39:26 +0300

    Start new course and add new repos.

---
+++ C# Advanced/01.Lab Stacks and Queues/03. Simple Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _03._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ C# Advanced/01.Lab Stacks and Queues/06. Supermarket/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _06._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
