d6aa3d2ee2f3165a26e9756f601dac5f71cc3e67
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-05-19 21:33:56 +0300

    Add new folder whit new solutions

---
+++ C# Advanced/04.Exercise Multidimensional Arrays/04.Exercise Multidimensional Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04.Exercise_Multidimensional_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
