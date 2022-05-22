9c1622968fded56abf9c3e54801101b518a2d165
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-05-22 17:58:38 +0300

    Update solutions

--- C# Advanced/03.Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
+++ C# Advanced/03.Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03._Primary_Diagonal
 {
@@ -6,7 +7,33 @@ namespace _03._Primary_Diagonal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int [,] numbers = new int[n, n];
+            int sum = 0;
+            FillMatrix(numbers);
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    int number = numbers[row, col];
+                    if (row == col)
+                    {
+                        sum += number;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+        private static void FillMatrix(int[,] numbers)
+        {
+            for (int row = 0; row < numbers.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < numbers.GetLength(1); col++)
+                {
+                    numbers[row, col] = rowData[col];
+                }
+            }
         }
     }
 }
