b3e6753dfe90276d5bdf2c12d01cd9adfca8a781
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-05-22 21:40:59 +0300

    Update solutios

--- C# Advanced/04.Exercise Multidimensional Arrays/03. Maximal Sum/Program.cs
+++ C# Advanced/04.Exercise Multidimensional Arrays/03. Maximal Sum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03._Maximal_Sum
 {
@@ -6,7 +7,44 @@ namespace _03._Maximal_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine().Split(new[] {' '},StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+            int[][] matrix = new int[rows][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                matrix[row] = Console.ReadLine().Split(new[] {' '}).Select(int.Parse).ToArray();
+            }
+            int biggerSum = 0;
+            int rowStartIndex = 0;
+            int colStartIndex = 0;
+
+            for (int row = 0; row < matrix.Length - 2; row++)
+            {
+                for (int col = 0; col < matrix[row].Length - 2; col++)
+                {
+                    int sum = matrix[row][col] + matrix[row][col + 1] + matrix[row][col + 2] +
+                        matrix[row + 1][col] + matrix[row + 1][col + 1] + matrix[row + 1][col + 2] +
+                        matrix[row + 2][col] + matrix[row + 2][col + 1] + matrix[row + 2][col + 2];
+
+                    if (sum > biggerSum)
+                    {
+                        biggerSum = sum;
+                        rowStartIndex = row;
+                        colStartIndex = col;
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {biggerSum}");
+            for (int row = 0; row < rowStartIndex + 2; row++)
+            {
+                for (int col = 0; col < colStartIndex + 2; col++)
+                {
+                    Console.Write($"{matrix[row][col]} ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- C# Advanced/04.Exercise Multidimensional Arrays/07. Knight Game/Program.cs
+++ C# Advanced/04.Exercise Multidimensional Arrays/07. Knight Game/Program.cs
@@ -1,12 +1,208 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
 
-namespace _07._Knight_Game
+
+
+//namespace _07._Knight_Game
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Queue<Tuple<int, int>> kniteMoves = new Queue<Tuple<int, int>>();
+//            kniteMoves.Enqueue(new Tuple<int, int>(-1, -2));
+//            kniteMoves.Enqueue(new Tuple<int, int>(+1, -2));
+//            kniteMoves.Enqueue(new Tuple<int, int>(-2, -1));
+//            kniteMoves.Enqueue(new Tuple<int, int>(+2, -1));
+//            kniteMoves.Enqueue(new Tuple<int, int>(-2, +1));
+//            kniteMoves.Enqueue(new Tuple<int, int>(+2, +1));
+//            kniteMoves.Enqueue(new Tuple<int, int>(-1, +2));
+//            kniteMoves.Enqueue(new Tuple<int, int>(+1, +2));
+//            int boardSize = int.Parse(Console.ReadLine());
+//            string[,] board = new string[boardSize, boardSize];
+//            for (int i = 0; i < boardSize; i++)
+//            {
+//                char[] curRow = Console.ReadLine().ToCharArray();
+//                for (int k = 0; k < boardSize; k++)
+//                {
+//                    board[i, k] = curRow[k].ToString();
+//                }
+//            }
+//            bool foundHit = true;
+//            int mostHits = 0;
+//            int saveMostHits = 0;
+//            int bestRow = int.MinValue;
+//            int bestCol = int.MinValue;
+//            int removedResult = 0;
+//            while (foundHit == true)
+//            {
+//                for (int i = 0; i < board.GetLength(0); i++)
+//                {
+//                    for (int k = 0; k < board.GetLength(1); k++)
+//                    {
+//                        if (board[i, k] == "K")
+//                        {
+//                            for (int j = 1; j <= kniteMoves.Count; j++)
+//                            {
+//                                int row = kniteMoves.Peek().Item1;
+//                                int col = kniteMoves.Peek().Item2;
+//                                kniteMoves.Enqueue(kniteMoves.Dequeue());
+//                                try
+//                                {
+//                                    if (board[i + row, k + col] == "K")
+//                                    {
+//                                        mostHits++;
+//                                    }
+//                                }
+//                                catch (Exception)
+//                                {
+//                                    continue;
+//                                }
+//                            }
+//                        }
+//                        if (mostHits > 0)
+//                        {
+//                            foundHit = true;
+//                        }
+//                        else
+//                        {
+//                            foundHit = false;
+//                        }
+//                        if (mostHits > saveMostHits)
+//                        {
+//                            bestRow = i;
+//                            bestCol = k;
+//                            saveMostHits = mostHits;
+//                        }
+//                        mostHits = 0;
+//                    }
+//                }
+//                if (foundHit = true && bestRow != int.MinValue && bestCol != int.MinValue)
+//                {
+//                    board[bestRow, bestCol] = "0";
+//                    removedResult++;
+//                }
+//                saveMostHits = 0;
+//                bestRow = int.MinValue;
+//                bestCol = int.MinValue;
+//            }
+//            Console.WriteLine(removedResult);
+//        }
+//    }
+//}
+
+
+using System;
+
+namespace knightGame
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            char[][] jagged = new char[n][];
+            for (int i = 0; i < n; i++)
+            {
+                jagged[i] = Console.ReadLine().ToCharArray();
+            }
+            int[,] possibleMooves = new int[n, n];
+            int removed = 0;
+            while (true)
+            {
+                int maxCountMoves = int.MinValue;
+                int row = 0;
+                int col = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (jagged[i][j] == 'K')
+                        {
+                            if (i - 1 > -1 && i - 1 < jagged.Length && j - 2 > -1 && j - 2 < jagged[i - 1].Length)
+                            {
+                                if (jagged[i - 1][j - 2] == 'K')
+                                {
+                                    possibleMooves[i, j]++;
+                                }
+                            }
+                            if (i - 1 > -1 && i - 1 < jagged.Length && j + 2 > -1 && j + 2 < jagged[i - 1].Length)
+                            {
+                                if (jagged[i - 1][j + 2] == 'K')
+                                {
+                                    possibleMooves[i, j]++;
+                                }
+                            }
+                            if (i + 1 > -1 && i + 1 < jagged.Length && j - 2 > -1 && j - 2 < jagged[i + 1].Length)
+                            {
+                                if (jagged[i + 1][j - 2] == 'K')
+                                {
+                                    possibleMooves[i, j]++;
+                                }
+                            }
+                            if (i + 1 > -1 && i + 1 < jagged.Length && j + 2 > -1 && j + 2 < jagged[i + 1].Length)
+                            {
+                                if (jagged[i + 1][j + 2] == 'K')
+                                {
+                                    possibleMooves[i, j]++;
+                                }
+                            }
+                            if (i - 2 > -1 && i - 2 < jagged.Length && j - 1 > -1 && j - 1 < jagged[i - 2].Length)
+                            {
+                                if (jagged[i - 2][j - 1] == 'K')
+                                {
+                                    possibleMooves[i, j]++;
+                                }
+                            }
+                            if (i - 2 > -1 && i - 2 < jagged.Length && j + 1 > -1 && j + 1 < jagged[i - 2].Length)
+                            {
+                                if (jagged[i - 2][j + 1] == 'K')
+                                {
+                                    possibleMooves[i, j]++;
+                                }
+                            }
+                            if (i + 2 > -1 && i + 2 < jagged.Length && j - 1 > -1 && j - 1 < jagged[i + 2].Length)
+                            {
+                                if (jagged[i + 2][j - 1] == 'K')
+                                {
+                                    possibleMooves[i, j]++;
+
+                                }
+                            }
+                            if (i + 2 > -1 && i + 2 < jagged.Length && j + 1 > -1 && j + 1 < jagged[i + 2].Length)
+                            {
+                                if (jagged[i + 2][j + 1] == 'K')
+                                {
+                                    possibleMooves[i, j]++;
+                                }
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (possibleMooves[i, j] > maxCountMoves)
+                        {
+                            maxCountMoves = possibleMooves[i, j];
+                            row = i;
+                            col = j;
+                        }
+                    }
+                }
+                if (maxCountMoves > 0)
+                {
+                    jagged[row][col] = '0';
+                    removed++;
+                    possibleMooves = new int[n, n];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(removed);
         }
     }
 }
