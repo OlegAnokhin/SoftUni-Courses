6fabf7bbdf67f9ed9911f28e8fdb994294b50f41
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-05-24 09:31:05 +0300

    Update solutions

--- C# Advanced/05.Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
+++ C# Advanced/05.Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -7,7 +7,36 @@ namespace _07._Parking_Lot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            HashSet<string> parking = new HashSet<string>();
+
+            while (input != "END")
+            {
+                string[] cmdArg = input.Split(", ");
+                string cmd = cmdArg[0];
+                string number = cmdArg[1];
+
+                if (cmd == "IN")
+                {
+                    parking.Add(number);
+                }
+                else
+                {
+                    parking.Remove(number);
+                }
+                input = Console.ReadLine();
+            }
+            if (parking.Count > 0)
+            {
+                foreach (string numbers in parking)
+                {
+                    Console.WriteLine(numbers);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
         }
     }
 }
--- C# Advanced/05.Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
+++ C# Advanced/05.Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace _08._SoftUni_Party
 {
@@ -7,7 +9,57 @@ namespace _08._SoftUni_Party
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+
+            HashSet<string> VIP = new HashSet<string>();
+            HashSet<string> regular = new HashSet<string>();
+            string pattern = @"(^\d)";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            while (input != "PARTY")
+            {
+                if (input.Length == 8)
+                {
+                    if (regex.IsMatch(input))
+                    {
+                        VIP.Add(input);
+                    }
+                    else
+                    {
+                        regular.Add(input);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (VIP.Contains(input))
+                {
+                    VIP.Remove(input);
+                }
+                else if (regular.Contains(input))
+                {
+                    regular.Remove(input);
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(VIP.Count + regular.Count);
+            if (VIP.Count > 0)
+            {
+                foreach (string vip in VIP)
+                {
+                    Console.WriteLine(vip);
+                }
+            }
+            if (regular.Count > 0)
+            {
+                foreach (string reg in regular)
+                {
+                    Console.WriteLine(reg);
+                }
+            }
         }
     }
 }
