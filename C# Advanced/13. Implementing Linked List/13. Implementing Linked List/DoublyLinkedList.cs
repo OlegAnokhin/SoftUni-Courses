9be305dbdc0e29a46b82084281461779e63a5f6a
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-06-06 20:51:08 +0300

    Add new folder and solutions

---
+++ C# Advanced/13. Implementing Linked List/13. Implementing Linked List/DoublyLinkedList.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Implementing_Linked_List
+{
+    public class DoublyLinkedList<T>
+    {
+        public int Count { get; set; }
+        public LinkedListNode Head { get; set; }
+        public LinkedListNode Tail { get; set; }
+
+        public bool IsEmpty => this.Count == 0;
+
+
+        public void AddHead(T value)
+        {
+            if (this.IsEmpty)
+            {
+                this.Head = this.Tail = new LinkedListNode(value);
+            }
+            else
+            {
+                var previousHead = this.Head;
+                var newNode = new LinkedListNode(value);
+                previousHead.Previous = newNode;
+                newNode.Next = previousHead;
+                this.Head = newNode;
+
+            }
+            this.Count++;
+        }
+        public void AddTail(T value)
+        {
+            if (this.IsEmpty)
+            {
+                this.Head = this.Tail = new LinkedListNode(value);
+            }
+            else
+            {
+                var previousTail = this.Tail;
+                var newNode = new LinkedListNode(value);
+                newNode.Previous = previousTail;
+                previousTail.Next = newNode;
+                this.Tail = newNode;
+            }
+            this.Count++;
+
+        }
+        public T RemoveHead()
+        {
+            if (this.IsEmpty)
+            {
+                throw new InvalidOperationException("List is empty.");
+            }
+            var removedHead = this.Head;
+            var removedHeadValue = removedHead.Value;
+            var nextHead = removedHead.Next;
+
+            if (nextHead == null)
+            {
+                this.Head = this.Tail = null;
+            }
+            else
+            {
+                nextHead.Previous = null;
+                removedHead.Next = null;
+                this.Head = nextHead;
+            }
+            this.Count--;
+
+            return removedHeadValue;
+        }
+        public T RemoveTail()
+        {
+            if (this.IsEmpty)
+            {
+                throw new InvalidOperationException("List is empty.");
+            }
+            var removedTail = this.Tail;
+            var removedTailValue = removedTail.Value;
+            var nextTail = removedTail.Previous;
+            if (nextTail == null)
+            {
+                this.Head = this.Tail = null;
+            }
+            else
+            {
+                removedTail.Previous = null;
+                nextTail.Next = null;
+                this.Tail = nextTail;
+            }
+            this.Count--;
+            return removedTailValue;
+        }
+        public void ForEach(Action<T> action)
+        {
+            var currentNode = this.Head;
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.Next;
+            }
+        }
+
+        public List<T> ToList()
+        {
+            var list = new List<T>();
+            this.ForEach(n => list.Add(n));
+            return list;
+        }
+        public T[] ToArray()
+        {
+            var array = new T[this.Count];
+            int counter = 0;
+
+            // option 1
+            //this.ForEach(number =>
+            //{
+            //    array[counter] = number;
+            //    counter++;
+            //});
+
+            // option 2
+
+            var currNode = this.Head;
+            while (currNode != null)
+            {
+                array[counter] = currNode.Value;
+                counter++;
+                currNode = currNode.Next;
+            }
+
+            return array;
+        }
+
+        public class LinkedListNode
+        {
+            public LinkedListNode(T value)
+            {
+                Value = value;
+            }
+            public T Value { get; set; }
+            public LinkedListNode Next { get; set; }
+            public LinkedListNode Previous { get; set; }
+        }
+    }
+}
---
+++ C# Advanced/13. Implementing Linked List/13. Implementing Linked List/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _Implementing_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var linkedList = new DoublyLinkedList<int>();
+
+            linkedList.AddHead(1);
+            linkedList.AddHead(2);
+            linkedList.AddHead(3);
+            linkedList.AddTail(4);
+
+            linkedList.ForEach(n => Console.WriteLine(n));
+
+            var list = linkedList.ToList();
+
+            var array = linkedList.ToArray();
+
+
+            Console.WriteLine($"Head; {linkedList.Head.Value}");
+            Console.WriteLine($"Tail; {linkedList.Tail.Value}");
+
+            Console.WriteLine($"Next to head {linkedList.Head.Next.Value}");
+            Console.WriteLine($"Previons to tail {linkedList.Head.Next.Value}");
+
+            var removedHead = linkedList.RemoveHead();
+            Console.WriteLine($"Removed head: {removedHead}");
+            Console.WriteLine($"Head after removal: {linkedList.Head.Value}");
+
+            var removedTail = linkedList.RemoveTail();
+            Console.WriteLine($"Removed tail: {removedTail}");
+            Console.WriteLine($"Tail after removal: {linkedList.Tail.Value}");
+
+            var horo = new DoublyLinkedList<string>();
+            horo.AddHead("Ivan");
+            horo.AddHead("Gergana");
+            horo.AddHead("Pesho");
+            horo.AddTail("Ivana");
+
+            horo.ForEach(p => Console.WriteLine(p));
+
+        }
+    }
+}
