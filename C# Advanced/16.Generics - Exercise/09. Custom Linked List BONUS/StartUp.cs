1919935a6b899a1a79ef13d7dac6073538be38ec
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-06-09 21:55:27 +0300

    Updated the decision

--- C# Advanced/16.Generics - Exercise/09. Custom Linked List BONUS/StartUp.cs
+++ C# Advanced/16.Generics - Exercise/09. Custom Linked List BONUS/StartUp.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace _09._Custom_Linked_List_BONUS
+namespace CustomDoublyLinkedList
 {
-    internal class StartUp
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var linkedList = new DoublyLinkedList<int>();
+
+            linkedList.AddFirst(1);
+            linkedList.AddFirst(2);
+            linkedList.AddFirst(3);
+            linkedList.AddLast(4);
+
+            linkedList.ForEach(n => Console.WriteLine(n));
+
+            var list = linkedList.ToList();
+
+            var array = linkedList.ToArray();
+
+
+            Console.WriteLine($"Head; {linkedList.Head.Value}");
+            Console.WriteLine($"Tail; {linkedList.Tail.Value}");
+
+            Console.WriteLine($"Next to head {linkedList.Head.Next.Value}");
+            Console.WriteLine($"Previons to tail {linkedList.Head.Next.Value}");
+
+            var removedHead = linkedList.RemoveFirst();
+            Console.WriteLine($"Removed head: {removedHead}");
+            Console.WriteLine($"Head after removal: {linkedList.Head.Value}");
+
+            var removedTail = linkedList.RemoveLast();
+            Console.WriteLine($"Removed tail: {removedTail}");
+            Console.WriteLine($"Tail after removal: {linkedList.Tail.Value}");
+
+            var horo = new DoublyLinkedList<string>();
+            horo.AddFirst("Ivan");
+            horo.AddFirst("Gergana");
+            horo.AddFirst("Pesho");
+            horo.AddLast("Ivana");
+
+            horo.ForEach(p => Console.WriteLine(p));
+
+
         }
     }
 }
---
+++ C# Advanced/16.Generics - Exercise/09. Custom Linked List BONUS/Tuple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    public class Tuple<TFirst, TSecond>
+    {
+        public Tuple(TFirst firstElement, TSecond secondElement)
+        {
+            FirstElement = firstElement;
+            SecondElement = secondElement;
+        }
+        public TFirst FirstElement { get; set; }
+        public TSecond SecondElement { get; set; }
+        public override string ToString()
+        {
+            return $"{FirstElement} -> {SecondElement}";
+        }
+    }
+}
