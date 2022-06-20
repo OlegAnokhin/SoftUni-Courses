37dcd24ab16f5f03db7d706c2d356d69e6bba5f2
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-06-20 21:45:12 +0300

    Add new folder with solutions

---
+++ C# Advanced/19.BasicAlgorithms-Exercise/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            long sum = SumArray(array, 0);
+            Console.WriteLine(sum);
+        }
+        private static long SumArray(int[] array, int startIndex)
+        {
+            if (startIndex == array.Length - 1)
+            {
+                return array[startIndex];
+            }
+            return array[startIndex] + SumArray(array, startIndex + 1);
+        }
+    }
+}
---
+++ C# Advanced/19.BasicAlgorithms-Exercise/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02.RecursiveFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
