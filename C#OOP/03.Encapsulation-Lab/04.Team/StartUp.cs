26c3939c962b311bf1cf19b5ea6e674b10bd9dc3
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-07-04 22:46:40 +0300

    Add new folder with new solutions

---
+++ C#OOP/03.Encapsulation-Lab/04.Team/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        decimal.Parse(cmdArgs[3]));
+
+                Team team = new Team("SoftUni");
+                foreach (Person perso in persons)
+                {
+                    team.AddPlayer(person);
+                }
+            }
+
+
+
+            // как се печата това съобщение????????
+
+            //Console.WriteLine($"First team has {CountFirst} players.");
+
+            //int firrs = Team.;
+
+            //return $"First team has {FirstTeam.Count} players.";
+            //return $"Reserve team has {ReserveTeam.Count} players.";
+
+        }
+    }
+}
---
+++ C#OOP/03.Encapsulation-Lab/04.Team/Team.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+        public IReadOnlyCollection<Person> FirstTeam 
+        { 
+            get
+            { return this.firstTeam.AsReadOnly(); }
+        }
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this.reserveTeam.AsReadOnly(); }
+        }
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+        public int CountFirst { get { return firstTeam.Count; } }
+        public int CountReserve { get { return reserveTeam.Count; } }
+        public override string ToString()
+        {
+            return $"First team has {FirstTeam.Count} players.";
+            return $"Reserve team has {ReserveTeam.Count} players.";
+        }
+
+    }
+}
