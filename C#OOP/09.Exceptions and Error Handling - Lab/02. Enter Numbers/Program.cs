25d305c9869db096d46ed0becd6224c3141ee4b2
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-07-17 10:35:11 +0300

    Upgrade solution

--- C#OOP/09.Exceptions and Error Handling - Lab/02. Enter Numbers/Program.cs
+++ C#OOP/09.Exceptions and Error Handling - Lab/02. Enter Numbers/Program.cs
@@ -5,112 +5,102 @@ namespace _02._Enter_Numbers
 {
     public class Program
     {
-        //        static void Main(string[] args)
-        //        {
-        //            int start = 1;
-        //            int end = 100;
-        //            int count = 0;
-
-        //            List<int> numbers = new List<int>();
-        //            while (count < 10)
-        //            {
-        //                start = ReadNumber(start, end, numbers, ref count);
-        //            }
-        //            Console.WriteLine(String.Join(", ", numbers));
-        //        }
-        //        public static int ReadNumber(int start, int end, List<int> numbers, ref int count)
-        //        {
-        //            count++;
-        //            int num = 0;
-        //            try
-        //            {
-        //                num = int.Parse(Console.ReadLine());
-        //                if (!(start < num && num < end))
-        //                {
-        //                    while (!(start < num && num < end))
-        //                    {
-        //                        throw new ArgumentException();
-        //                    }
-        //                }
-        //                numbers.Add(num);
-        //            }
-        //            catch (FormatException)
-        //            {
-        //                count--;
-        //                Console.WriteLine("Invalid Number!");
-        //            }
-        //            catch (ArgumentException)
-        //            {
-        //                count--;
-        //                Console.WriteLine($"Your number is not in range {start} - {end}!");
-        //            }
-        //            catch
-        //            {
-        //                count--;
-        //                Console.WriteLine("Invalid Number!");
-        //            }
-        //            return num;
-        //        }
-        //    }
-        //}
-
-
-        static void Main()
+        static void Main(string[] args)
         {
             int start = 1;
             int end = 100;
-            int[] numbers = new int[10];
+            int count = 0;
 
-            for (int i = 0; i < 10; i++)
+            List<int> numbers = new List<int>();
+            while (count < 10)
             {
-                while (numbers[i] <= start || numbers[i] >= end || (i > 0 && numbers[i] <= numbers[i - 1]))
-                {
-                    bool exeption = false;
-                    try
-                    {
-                        numbers[i] = int.Parse(Console.ReadLine());
-                        if ((numbers[i] <= start || numbers[i] >= end) && !exeption)
-                        {
-                            throw new ArgumentException();
-                            //  Console.WriteLine($"Your number is not in range {numbers[i - 1]} - 100!");
-                        }
-                        else if (i > 0 && numbers[i] <= numbers[i - 1] && !exeption)
-                        {
-                            throw new ArgumentException();
-                            //  Console.WriteLine($"Number must be bigger than {numbers[i - 1]}!");
-                        }
-                    }
-                    catch (ArgumentException)
-                    {
-                        if (numbers[i] <= 1)
-                        {
-                            Console.WriteLine($"Your number is not in range {numbers[i]} - 100!");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Your number is not in range {numbers[i - 1]} - 100!");
-                        }
-                    }
-                    catch (Exception exc)
-                    {
-                        Console.WriteLine("Invalid Number!");
-                        exeption = true;
-                    }
-                    //if ((numbers[i] <= start || numbers[i] >= end) && !exeption)
-                    //{
-                    //    Console.WriteLine($"Your number is not in range {numbers[i - 1]} - 100!");
-                    //}
-                    //else if (i > 0 && numbers[i] <= numbers[i - 1] && !exeption)
-                    //{
-                    //    Console.WriteLine($"Number must be bigger than {numbers[i - 1]}!");
-                    //}
-                }
-                if (numbers[i] == 99)
-                {
-                    break;
-                }
+                start = ReadNumber(start, end, numbers, ref count);
             }
             Console.WriteLine(String.Join(", ", numbers));
         }
+        public static int ReadNumber(int start, int end, List<int> numbers, ref int count)
+        {
+            count++;
+            int num = 0;
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+                if (!(start < num && num < end))
+                {
+                    while (!(start < num && num < end))
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+                numbers.Add(num);
+            }
+            catch (FormatException)
+            {
+                count--;
+                Console.WriteLine("Invalid Number!");
+            }
+            catch (ArgumentException)
+            {
+                count--;
+                Console.WriteLine($"Your number is not in range {start} - {end}!");
+            }
+            catch
+            {
+                count--;
+                Console.WriteLine("Invalid Number!");
+            }
+            return num;
+        }
     }
 }
+
+
+//        static void Main()
+//        {
+//            int start = 1;
+//            int end = 100;
+//            int[] numbers = new int[10];
+
+//            for (int i = 0; i < 10; i++)
+//            {
+//                while (numbers[i] <= start || numbers[i] >= end || (i > 0 && numbers[i] <= numbers[i - 1]))
+//                {
+//                    bool exeption = false;
+//                    try
+//                    {
+//                        numbers[i] = int.Parse(Console.ReadLine());
+//                        if ((numbers[i] <= start || numbers[i] >= end) && !exeption)
+//                        {
+//                            throw new ArgumentException();
+//                        }
+//                        else if (i > 0 && numbers[i] <= numbers[i - 1] && !exeption)
+//                        {
+//                            throw new ArgumentException();
+//                        }
+//                    }
+//                    catch (ArgumentException)
+//                    {
+//                        if (numbers[i] == 1 || numbers[i] == 100)
+//                        {
+//                            Console.WriteLine($"Your number is not in range {numbers[i]} - 100!");
+//                        }
+//                        else
+//                        {
+//                            Console.WriteLine($"Your number is not in range {numbers[i - 1]} - 100!");
+//                        }
+//                    }
+//                    catch (Exception exc)
+//                    {
+//                        Console.WriteLine("Invalid Number!");
+//                        exeption = true;
+//                    }
+//                }
+//                if (numbers[i] == 99)
+//                {
+//                    break;
+//                }
+//            }
+//            Console.WriteLine(String.Join(", ", numbers));
+//        }
+//    }
+//}
--- C#OOP/09.Exceptions and Error Handling - Lab/06. Money Transactions/Program.cs
+++ C#OOP/09.Exceptions and Error Handling - Lab/06. Money Transactions/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _06._Money_Transactions
 {
@@ -6,7 +8,88 @@ namespace _06._Money_Transactions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split(",");
+            Dictionary<int, double> accountPair = new Dictionary<int, double>();
+            foreach (var line in input)
+            {
+                var accPair = line.Split("-").Select(double.Parse).ToArray();
+                int account = (int)accPair[0];
+                double balance = (double)accPair[1];
+                accountPair.Add(account, balance);
+            }
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                try
+                {
+                    string[] cmdArg = command.Split();
+                    string cmdType = cmdArg[0];
+                    int accNumber = int.Parse(cmdArg[1]);
+                    double sum = double.Parse(cmdArg[2]);
+                    double newBalance = 0;
+                    if (cmdType == "Deposit")
+                    {
+                        if (accountPair.ContainsKey(accNumber))
+                        {
+                            accountPair[accNumber] += sum;
+                            newBalance = GetValue(accountPair, accNumber, newBalance);
+                            Console.WriteLine($"Account {accNumber} has new balance: {newBalance:f2}");
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+                    }
+                    else if (cmdType == "Withdraw")
+                    {
+                        if (accountPair.ContainsKey(accNumber))
+                        {
+                            if (accountPair[accNumber] - sum < 0)
+                            {
+                                throw new ArgumentNullException();
+                            }
+                            accountPair[accNumber] -= sum;
+                            newBalance = GetValue(accountPair, accNumber, newBalance);
+                            Console.WriteLine($"Account {accNumber} has new balance: {newBalance:f2}");
+                        }
+                        else
+                        {
+                            throw new Exception();
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Insufficient balance!");
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Invalid command!");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid account!");
+                }
+                finally
+                {
+                    Console.WriteLine("Enter another command");
+                }
+            }
+        }
+        public static double GetValue(Dictionary<int, double> accountPair, int accNumber, double newBalance)
+        {
+            foreach (var kvp in accountPair)
+            {
+                if (kvp.Key == accNumber)
+                {
+                    newBalance = kvp.Value;
+                }
+            }
+            return newBalance;
         }
     }
 }
