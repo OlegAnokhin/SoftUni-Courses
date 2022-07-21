30274d5ef30c8ecc4fab6754e6eeae026bf0d50e
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-07-21 22:09:12 +0300

    Add new folder with solutions

---
+++ C#OOP/12.Reflection and Attributes - Lab/05.CreateAttribute/AuthorProblem.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthorProblem
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class AuthorProblem : Attribute
+    {
+        public string Name { get; set; }
+        public AuthorProblem(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
