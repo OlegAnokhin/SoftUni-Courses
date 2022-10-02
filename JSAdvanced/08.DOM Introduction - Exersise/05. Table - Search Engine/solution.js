ec55a6dc718944d6627cee203fe7b7eaa4bea848
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-10-02 21:00:15 +0300

    Upgrade solution

--- JSAdvanced/08.DOM Introduction - Exersise/05. Table - Search Engine/solution.js
+++ JSAdvanced/08.DOM Introduction - Exersise/05. Table - Search Engine/solution.js
@@ -1,8 +1,16 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let fieldList = Array.from(document.querySelectorAll('tbody tr'));
+   let searchText = document.getElementById('searchField');
 
    function onClick() {
-      //   TODO:
-
+      for (let row of fieldList) {
+         row.classList.remove('select');
+         if (row.textContent.includes(searchText.value)) {
+            row.className = 'select';
+         }
+      };
+      searchText.value = '';
+      debugger
    }
 }
