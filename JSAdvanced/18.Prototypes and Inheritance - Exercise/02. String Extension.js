d11f75dd744cb71489341258bfddb0923826001d
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-10-17 15:42:18 +0300

    Upgrade of solution

--- JSAdvanced/18.Prototypes and Inheritance - Exercise/02. String Extension.js
+++ JSAdvanced/18.Prototypes and Inheritance - Exercise/02. String Extension.js
@@ -1,32 +1,60 @@
-//(
+(
     function stringExtension() {
-    Array.prototype.ensureStart = function (str) {
-        return this[this.length - 1];
-    }
-    Array.prototype.ensureEnd = function (str) {
-        return this.slice(n);
-    }
-    Array.prototype.isEmpty = function () {
-        return this.slice(0, n)
-    }
-    Array.prototype.truncate = function (n) {
-        return this.reduce((prevValue , nextValue) => prevValue + nextValue);
-    }
-    Array.prototype.format = function (string, ...params) {
-        return this.sum() / this.length;
+    String.prototype.ensureStart = function (str) {
+        return this.startsWith(str) ? `${this}` : str + this;    
+    };
+    String.prototype.ensureEnd = function (str) {
+        return this.endsWith(str) ? `${this}` : this + str;
+    };
+    String.prototype.isEmpty = function () {
+        return this.length == 0 ? true : false;
+    };
+    String.prototype.truncate = function (n) {
+        if (this.length <= n) {
+            return this.toString();
+        };
+        if (n < 4) {
+            return '.'.repeat(n);
+        };
+        let input = this.split(' ');
+        while ((input.join(' ') + '...').length > n) {
+            if (input.length > 1) {
+                input.pop();
+            }else {
+                input[0] = input[0].slice(0, n - 3);
+            }
+        };
+        return input.join(' ').trim() + '...';
+    };
+    String.format = function (str, ...params) {
+        params.forEach((value, index) => str = str.replace(`{${index}}`.toString(), value));
+        return str;
     }
 
-    let str = 'my string';
-    str = str.ensureStart('my');
-    str = str.ensureStart('hello ');
-    str = str.truncate(16);
-    str = str.truncate(14);
-    str = str.truncate(8);
-    str = str.truncate(4);
-    str = str.truncate(2);
-    str = String.format('The {0} {1} fox','quick', 'brown');
-    str = String.format('jumps {0} {1}','dog');
+    // let str = 'my string';
+    // str = str.ensureStart('my');
+    // console.log(str);
+    // str = str.ensureStart('hello ');
+    // console.log(str);
+    // str = str.ensureEnd(' hello');
+    // console.log(str)
+    // str = str.isEmpty();
+    // console.log(str)
+    // str = str.truncate(16);
+    // console.log(str)
+    // str = str.truncate(14);
+    // console.log(str)
+    // str = str.truncate(8);
+    // console.log(str)
+    // str = str.truncate(4);
+    // console.log(str)
+    // str = str.truncate(2);
+    // console.log(str)
+    // str = String.format('The {0} {1} fox', 'quick', 'brown');
+    // console.log(str)
+    // str = String.format('jumps {0} {1}', 'dog');
+    // console.log(str)
 }
-//)()
+)()
 
-stringExtension();
+//stringExtension();
