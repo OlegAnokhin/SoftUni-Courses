120c895fdef08ba129da182055d73ca314429326
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-10-20 19:02:09 +0300

    Add new folder with solution

---
+++ JSAdvanced/19.ExamPreparation/Exam13.03/03. Rent Car/tests.test.js
@@ -0,0 +1,20 @@
+let rentCar = require(`./rentCar`);
+let {expect, assert} = require('chai');
+
+describe("Tests Rent Car", function() {
+    describe("test searchCar", function() {
+        it("TODO …", function() {
+            // TODO: …
+        });
+     });
+     describe("test calculatePriceOfCar", function() {
+        it("TODO …", function() {
+            // TODO: …
+        });
+     });
+     describe("test checkBudget", function() {
+        it("TODO …", function() {
+            // TODO: …
+        });
+     });
+});
