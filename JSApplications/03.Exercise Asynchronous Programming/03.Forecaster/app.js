3e4a9de68c30d67dac8d3d3426b4b1fe0f6e5e10
Oleg Anokhin <oleg_anokhin@abv.bg>
2022-10-31 22:34:24 +0200

    Add new folder with solution

---
+++ JSApplications/03.Exercise Asynchronous Programming/03.Forecaster/app.js
@@ -0,0 +1,114 @@
+const enumItem = {
+    "Sunny": "&#x2600", // ☀
+    "Partly sunny": "&#x26C5", // ⛅
+    "Overcast": "&#x2601", // ☁
+    "Rain": "&#x2614", // ☂
+    "Degrees": "&#176" // °
+}
+const forecastContainer = document.getElementById('forecast');
+function attachEvents() {
+    document.getElementById('submit').addEventListener('click', getWeather);
+}
+async function getWeather() {
+    const currSec = document.getElementById('current');
+    const upcomingContainer = document.getElementById('upcoming');
+    const forecastContainer = document.getElementById('forecast');
+
+    try {
+        const url = `http://localhost:3030/jsonstore/forecaster/locations`;
+        const townName = document.getElementById('location').value;
+        const respons = await fetch(url);
+        const data = await respons.json();
+        const info = data.find(x => x.name === townName);
+        const [todayData, upcomingData] = await Promise.all([
+            getToday(info.code),
+            getUpcoming(info.code)
+        ])
+        forecastContainer.style.display = 'block';
+        const todayHTMLTemp = createToday(todayData);
+        currSec.appendChild(todayHTMLTemp);
+        const upcomingHTMLTemp = createUpcoming(upcomingData);
+        upcomingContainer.appendChild(upcomingHTMLTemp);
+    } catch (e){
+        forecastContainer.style.display = 'block';
+        document.querySelector('.label').textContent = 'Error'
+    }
+}
+async function getToday(code) {
+    const urlToday = `http://localhost:3030/jsonstore/forecaster/today/${code}`;
+    const responsToday = await fetch(urlToday);
+    const dataToday = await responsToday.json();
+    return dataToday;
+}
+async function getUpcoming(code) {
+    const urlThreeDays = `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`
+    const responsThree = await fetch(urlThreeDays);
+    const dataThree = await responsThree.json();
+    return dataThree;
+}
+function createToday(data) {
+
+    const { condition, high, low } = data.forecast;
+    const conditionContainer = document.createElement('div');
+    conditionContainer.classList.add('forecasts');
+
+    const conditionIconSpan = document.createElement('span');
+    conditionIconSpan.classList.add('condition symbol');
+    conditionIconSpan.innerHTML = enumItem[condition];
+
+    const conditionSpan = document.createElement('span');
+    conditionSpan.classList.add('condition');
+
+    const nameSpan = document.createElement('span');
+    nameSpan.classList.add('forecast-data');
+    nameSpan.textContent = data.name;
+
+    const tempSpan = document.createElement('span');
+    tempSpan.classList.add('forecast-data');
+    tempSpan.innerHTML = `${low}${enumItem['Degrees']}/${high}${enumItem['Degrees']}`;
+
+    const conditionTxtSpan = document.createElement('span');
+    conditionTxtSpan.classList.add('forecast-data');
+    conditionTxtSpan.textContent = condition;
+
+    conditionSpan.appendChild(nameSpan);
+    conditionSpan.appendChild(tempSpan);
+    conditionSpan.appendChild(conditionTxtSpan);
+
+    conditionContainer.appendChild(conditionIconSpan);
+    conditionContainer.appendChild(conditionSpan);
+    return conditionContainer;
+}
+function createUpcoming(data) {
+    const container = document.createElement('div');
+    container.classList.add('forecast-info');
+    data.forecast.forEach(data => {
+        const spanHolder = generateSpans(data);
+        container.appendChild(spanHolder)
+    });
+    return container;
+}
+function generateSpans(data) {
+    const { condition, high, low } = data
+    const spanHolder = document.createElement('span');
+    spanHolder.classList.add('upcoming');
+
+    const iconSpan = document.createElement('span');
+    iconSpan.classList.add('symbol');
+    iconSpan.innerHTML = enumItem[condition];
+
+    const tempSpan = document.createElement('span');
+    tempSpan.classList.add('forecast-data');
+    tempSpan.innerHTML = `${low}${enumItem['Degrees']}/${high}${enumItem['Degrees']}`;
+
+    const conditionSpan = document.createElement('span');
+    conditionSpan.classList.add('forecast-data');
+    conditionSpan.textContent = condition;
+
+    spanHolder.appendChild(iconSpan);
+    spanHolder.appendChild(tempSpan);
+    spanHolder.appendChild(conditionSpan);
+    return spanHolder;
+}
+
+attachEvents();
---
+++ JSApplications/03.Exercise Asynchronous Programming/06.Blog/app.js
@@ -0,0 +1,49 @@
+function attachEvents() {
+    document.getElementById('btnLoadPosts').addEventListener('click', getPost);
+    document.getElementById('btnViewPost').addEventListener('click', getComments);
+}
+
+async function getPost() {
+    const selectOp = document.getElementById('posts');
+    const url = `http://localhost:3030/jsonstore/blog/posts`;
+    selectOp.innerHTML = '';
+    const respons = await fetch(url);
+    const data = await respons.json();
+    Object.values(data).forEach(post => {
+        const op = document.createElement('option');
+        op.value = post.id;
+        op.textContent = post.title;
+        selectOp.appendChild(op);
+    })
+}
+
+async function getComments() {
+    const postsUrl = `http://localhost:3030/jsonstore/blog/posts`;
+    const postsCommentsUrl = `http://localhost:3030/jsonstore/blog/comments`;
+
+    const selectedOp = document.getElementById('posts').selectedOptions[0].value;
+    const titleElement = document.getElementById('post-title');
+    const postBodyElement = document.getElementById('post-body');
+    const postUlElement = document.getElementById('post-comments');
+    postUlElement.innerHTML = '';
+
+    const postResponse = await fetch(postsUrl);
+    const postData = await postResponse.json();
+
+    const commentsRespons = await fetch(postsCommentsUrl);
+    const commentsData = await commentsRespons.json();
+
+    const selectedPOst = Object.values(postData).find(post => post.id == selectedOp);
+    titleElement.textContent = selectedPOst.title;
+    postBodyElement.textContent = selectedPOst.body;
+
+    const comments = Object.values(commentsData).filter(c => c.postId === selectedOp);
+    comments.forEach(c => {
+        const li = document.createElement('li');
+        li.textContent = c.text;
+        postUlElement.appendChild(li);
+    })
+}
+
+
+attachEvents();
