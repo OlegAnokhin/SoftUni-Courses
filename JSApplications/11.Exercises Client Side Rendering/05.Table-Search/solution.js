a7f2bc7c667f8144a35a2b9dd3cc53ac2eff476e
Oleg Anokhin <102062768+OlegAnokhin@users.noreply.github.com>
2022-11-20 21:45:28 +0200

    Update solution

--- JSApplications/11.Exercises Client Side Rendering/05.Table-Search/solution.js
+++ JSApplications/11.Exercises Client Side Rendering/05.Table-Search/solution.js
@@ -1,8 +1,33 @@
-function solve() {
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { get } from './api.js';
+
+async function solve() {
+   let result = await get();
+   let fieldList = document.querySelector('tbody');
+   let element = document.createDocumentFragment();
+   element = html`
+   ${result.map(student => html`
+   <tr>
+      <td>${student.firstName} ${student.lastName}</td>
+      <td>${student.email}</td>
+      <td>${student.course}</td>
+   </tr>
+   `)}`
+   render(element, fieldList);
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
-
+   function onClick(e) {
+      let searchField = document.getElementById('searchField');
+      let searchText = searchField.value;
+      Array.from(fieldList.querySelectorAll('tr'))
+         .forEach(row => {
+            let currRow = Array.from(row.children)
+            .map(child => child.textContent).join(' ');
+            currRow.toLowerCase().includes(searchText.toLowerCase())
+               ? row.setAttribute('class', 'select')
+               : row.removeAttribute('class', 'select');
+         })
+      searchField.value = '';
    }
 }
+solve()
