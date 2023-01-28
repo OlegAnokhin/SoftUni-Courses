03dc289ae33a28b145cb1279becb084d8e1c935f
Oleg Anokhin <oleg_anokhin@abv.bg>
2023-01-28 19:25:01 +0200

    Upgrade solution

--- C# DB MSSQL/06.IndicesAndDataAggregation/Indices and Data Aggregation.sql
+++ C# DB MSSQL/06.IndicesAndDataAggregation/Indices and Data Aggregation.sql
@@ -17,16 +17,15 @@ FROM [WizzardDeposits] AS wd
 GROUP BY wd.[DepositGroup]
 
 --04
---SELECT TOP 2 
---	d.DepositGroup 
---FROM
---(SELECT 
---	wd.DepositGroup,
---	AVG(wd.[MagicWandSize]) AS LongestMagicWand
---FROM [WizzardDeposits] AS wd
---GROUP BY wd.DepositGroup
---) AS d
---ORDER BY DepositGroup DESC
+SELECT TOP 2 
+	DepositGroup 
+FROM
+(SELECT 
+	DepositGroup,
+	AVG([MagicWandSize]) AS Average
+FROM [WizzardDeposits]
+GROUP BY [DepositGroup]) AS d
+ORDER BY Average ASC
 
 --05
 SELECT
@@ -82,17 +81,17 @@ ORDER BY [MagicWandCreator] ASC, [DepositGroup] ASC
 
 --TEST 02
 SELECT
-	*, COUNT(*) AS WizardCount 
+	[AgeGroup], COUNT(*) AS WizardCount 
 FROM
-(
-SELECT CASE
-	   WHEN ([Age] <= 10) THEN                  '[0-10]'
-       WHEN ([Age] >= 11 AND [Age] <= 20) THEN  '[11-20]'
-       WHEN ([Age] >= 21 AND [Age] <= 30) THEN  '[21-30]'
-       WHEN ([Age] >= 31 AND [Age] <= 40) THEN  '[31-30]'
-       WHEN ([Age] >= 41 AND [Age] <= 50) THEN  '[41-30]'
-       WHEN ([Age] >= 51 AND [Age] <= 60) THEN  '[51-30]'
-       WHEN ([Age] >= 61) THEN                  '[61+]'
+( SELECT [Age],
+	   CASE
+			WHEN [Age] BETWEEN 0 AND 10 THEN  '[0-10]'
+			WHEN [Age] BETWEEN 11 AND 20 THEN '[11-20]'
+			WHEN [Age] BETWEEN 21 AND 30 THEN '[21-30]'
+			WHEN [Age] BETWEEN 31 AND 40 THEN '[31-40]'
+			WHEN [Age] BETWEEN 41 AND 50 THEN '[41-50]'
+			WHEN [Age] BETWEEN 51 AND 60 THEN '[51-60]'
+			ELSE                              '[61+]'
 	   END AS [AgeGroup]
 FROM [WizzardDeposits] ) AS wd
 GROUP BY [AgeGroup]
@@ -114,4 +113,54 @@ WHERE [DepositStartDate] > '01/01/1985'
 GROUP BY [DepositGroup], [IsDepositExpired]
 ORDER BY [DepositGroup] DESC, [IsDepositExpired] ASC
 
+--12
+
+
 SELECT * FROM WizzardDeposits
+
+--13
+SELECT 
+	[DepartmentID],
+	SUM(Salary) AS TotalSalary
+FROM [Employees]
+GROUP BY [DepartmentID]
+ORDER BY [DepartmentID]
+
+--14
+SELECT 
+	[DepartmentID],
+	MIN(Salary) AS MinimumSalary
+FROM [Employees] AS e
+WHERE e.[DepartmentID] IN (2, 5, 7) AND e.[HireDate] > 01/01/2000
+GROUP BY [DepartmentID]
+
+--15 NOT WORK CORRECT FOR JUDGE
+SELECT * 
+FROM Employees
+WHERE Salary > 30000 AND [ManagerID] <> 42
+
+UPDATE [Employees]
+SET [Salary] = [Salary] + 5000
+WHERE DepartmentID = 1
+--SELECT [Salary] FROM Employees
+
+SELECT
+	[DepartmentID],
+	AVG(Salary) AS AverageSalary
+FROM [Employees]
+GROUP BY [DepartmentID]
+
+--16
+SELECT 
+	[DepartmentID],
+	MAX(Salary) AS MaxSalary
+FROM [Employees]
+GROUP BY [DepartmentID]
+HAVING MAX(Salary) NOT BETWEEN 30000 AND 70000
+
+--17
+SELECT 
+	COUNT(Salary) AS Count
+FROM [Employees]
+WHERE [ManagerID] IS NULL
+GROUP BY [ManagerID]
