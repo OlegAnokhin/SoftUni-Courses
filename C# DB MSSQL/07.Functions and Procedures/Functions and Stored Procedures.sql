5d10897f8385cc0a0d9eb8a7406e2f32a8dfa115
Oleg Anokhin <oleg_anokhin@abv.bg>
2023-02-02 22:12:58 +0200

    Update solution

--- C# DB MSSQL/07.Functions and Procedures/Functions and Stored Procedures.sql
+++ C# DB MSSQL/07.Functions and Procedures/Functions and Stored Procedures.sql
@@ -1,6 +1,6 @@
 
 --01
-CREATE PROCEDURE dbo.usp_GetEmployeesSalaryAbove35000
+CREATE PROCEDURE [dbo].[usp_GetEmployeesSalaryAbove35000]
 AS
 SELECT 
 	[FirstName], 
@@ -11,8 +11,8 @@ WHERE [Salary] > 35000
 EXEC [dbo].[usp_GetEmployeesSalaryAbove35000]
 
 --02
-CREATE PROCEDURE dbo.usp_GetEmployeesSalaryAboveNumber
-(@number DECIMAL(18,4) = 48100)
+CREATE PROCEDURE [dbo].[usp_GetEmployeesSalaryAboveNumber]
+(@number DECIMAL(18,4))
 AS
 SELECT 
 	[FirstName], 
@@ -20,10 +20,10 @@ SELECT
 FROM Employees
 WHERE [Salary] >= @number
 
-EXEC [dbo].[usp_GetEmployeesSalaryAboveNumber]
+EXEC [dbo].[usp_GetEmployeesSalaryAboveNumber] 48100
 
 --03
-CREATE PROCEDURE dbo.usp_GetTownsStartingWith
+CREATE PROCEDURE [dbo].[usp_GetTownsStartingWith]
 (@string VARCHAR(10))
 AS
 SELECT 
@@ -34,7 +34,7 @@ WHERE LEFT([Name], LEN(@string)) = @string
 EXEC [dbo].[usp_GetTownsStartingWith] 'b'
 
 --04
-CREATE PROCEDURE dbo.usp_GetEmployeesFromTown
+CREATE PROCEDURE [dbo].[usp_GetEmployeesFromTown]
 (@TownName VARCHAR(50))
 AS
 SELECT 
@@ -50,7 +50,7 @@ WHERE t.Name = @TownName
 EXEC [dbo].[usp_GetEmployeesFromTown] 'Sofia'
 
 --05
-CREATE OR ALTER FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+CREATE OR ALTER FUNCTION [ufn_GetSalaryLevel](@salary DECIMAL(18,4))
 RETURNS VARCHAR(10)
 AS
 BEGIN
@@ -70,29 +70,29 @@ SELECT
 FROM [Employees]
 
 --06
-CREATE PROCEDURE dbo.usp_EmployeesBySalaryLevel
+CREATE PROCEDURE [dbo].[usp_EmployeesBySalaryLevel]
 (@string VARCHAR(10))
 AS
 SELECT 
 	[FirstName],
 	[LastName]
 FROM [Employees]
-WHERE (SELECT dbo.ufn_GetSalaryLevel([Salary])) = @string
+WHERE [dbo].[ufn_GetSalaryLevel]([Salary]) = @string
 
 EXEC [dbo].[usp_EmployeesBySalaryLevel] 'High'
 
 --07
-CREATE FUNCTION ufn_IsWordComprised
+CREATE FUNCTION [ufn_IsWordComprised]
 (@setOfLetters VARCHAR(50), @word VARCHAR(50))
 RETURNS BIT
 AS
 BEGIN
-DECLARE @bool INT = 1
-WHILE @bool <= LEN(@word)
+DECLARE @index INT = 1
+WHILE @index <= LEN(@word)
 BEGIN
-	IF (CHARINDEX(SUBSTRING(@word, @bool, 1), @setOfLetters)) = 0
+	IF (CHARINDEX(SUBSTRING(@word, @index, 1), @setOfLetters)) = 0
 	RETURN 0
-	SET @bool += 1
+	SET @index += 1
 	END
 	RETURN 1
 END
@@ -101,9 +101,51 @@ SELECT dbo.ufn_IsWordComprised ('oistmiahf', 'Sofia')
 SELECT dbo.ufn_IsWordComprised ('oistmiahf', 'halves')
 
 --08 BONUS
+CREATE PROCEDURE usp_DeleteEmployeesFromDepartment @departmentId INT
+AS
+BEGIN
+	DECLARE @emploeesToDelete TABLE ([Id] INT)
+	INSERT INTO @emploeesToDelete
+		SELECT [EmployeeID]
+		FROM [Employees]
+		WHERE [DepartmentID] = @departmentId
+
+	DELETE
+	FROM [EmployeesProjects]
+	WHERE [EmployeeID] IN (SELECT * FROM @emploeesToDelete)
+
+	ALTER TABLE [Departments]
+	ALTER COLUMN [ManagerID] INT
+
+	UPDATE [Departments]
+	SET [ManagerID] = NULL
+	WHERE [ManagerID] IN (SELECT * FROM @emploeesToDelete)
+
+	UPDATE [Employees]
+	SET [ManagerID] = NULL
+	WHERE [ManagerID] IN (SELECT * FROM @emploeesToDelete)
+
+	DELETE
+	FROM [Employees]
+	WHERE [DepartmentID] = @departmentId
+
+	DELETE
+	FROM [Departments]
+	WHERE [DepartmentID] = @departmentId
+
+	SELECT COUNT (*)
+	FROM [Employees]
+	WHERE [DepartmentID] = @departmentId
+END
+
+EXEC [dbo].[usp_DeleteEmployeesFromDepartment] 7
+ 
+SELECT *
+ FROM [Employees]
+ WHERE [DepartmentID] = 7
 
 --09
-CREATE PROCEDURE dbo.usp_GetHoldersFullName
+CREATE PROCEDURE [dbo].[usp_GetHoldersFullName]
 AS 
 SELECT 
 CONCAT([FirstName], ' ', [LastName]) AS [Full Name]
@@ -112,7 +154,7 @@ FROM [AccountHolders]
 EXEC dbo.usp_GetHoldersFullName
 
 --10
-CREATE PROCEDURE dbo.usp_GetHoldersWithBalanceHigherThan
+CREATE PROCEDURE [dbo].[usp_GetHoldersWithBalanceHigherThan]
 (@number DECIMAL(18,4))
 AS
 SELECT 
@@ -128,11 +170,61 @@ ORDER BY FirstName, LastName
 EXEC [dbo].[usp_GetHoldersWithBalanceHigherThan] 30000
 
 --11
+CREATE FUNCTION ufn_CalculateFutureValue
+(@sum DECIMAL(18, 4), @earlyInterestRate FLOAT, @numberOfYears INT)
+RETURNS DECIMAL(18, 4)
+AS
+BEGIN
+	DECLARE @FutureValue DECIMAL(18, 4)
+	SET @FutureValue = @sum * POWER((1 + @earlyInterestRate), @numberOfYears)
+RETURN @FutureValue
+END
 
+SELECT dbo.ufn_CalculateFutureValue (1000, 0.1, 5)
 
-
-
-
+--12
+CREATE PROCEDURE [dbo].[usp_CalculateFutureValueForAccount]
+(@accountId INT , @rate FLOAT)
+AS
 SELECT 
-	*
-FROM [Accounts]
+	[a].[Id] AS [Account Id],
+	[ah].[FirstName] AS [First Name],
+	[ah].[LastName] AS [Last Name],
+	[a].[Balance] AS [Current Balance],
+	(SELECT dbo.ufn_CalculateFutureValue ([a].[Balance], @rate, 5)) AS [Balance in 5 years]
+FROM [Accounts] AS [a]
+JOIN [AccountHolders] AS [ah]
+ON [ah].[Id] = [a].[AccountHolderId]
+WHERE [a].[Id] = @accountId
+
+EXEC [dbo].[usp_CalculateFutureValueForAccount] 1, 0.1
+
+SELECT * FROM Accounts
+SELECT * FROM AccountHolders
+
+--13 BONUS
+CREATE FUNCTION ufn_CashInUsersGames
+(@gameName NVARCHAR(50))
+RETURNS TABLE
+AS
+RETURN
+	(
+	SELECT SUM ([Cash])
+	AS [SumCash]
+	FROM 
+	(
+		SELECT 
+		[g].[Name],
+		[ug].[Cash],
+		ROW_NUMBER() OVER (ORDER BY [ug].[Cash] DESC)
+		AS [RowNumber]
+		FROM [UsersGames] AS [ug]
+		JOIN [Games] AS [g]
+		ON [ug].[GameId] = [g].[Id]
+		WHERE [g].[Name] = @gameName
+	)
+	AS [RankingSubQuery]
+	WHERE [RowNumber] % 2 <> 0
+	)
+
+SELECT * FROM [dbo].[ufn_CashInUsersGames]('Love in a mist')
