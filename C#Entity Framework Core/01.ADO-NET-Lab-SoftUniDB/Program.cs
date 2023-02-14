4010a77819b7e2f32c9b072a9404236999a0c36d
Oleg Anokhin <oleg_anokhin@abv.bg>
2023-02-14 22:00:35 +0200

    Add new folder with solutions

---
+++ C#Entity Framework Core/01.ADO-NET-Lab-SoftUniDB/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.Data.SqlClient;
+
+SqlConnection connection = new SqlConnection("Server = ACERPREDATOR\\SQLEXPRESS; Database = SoftUni; Integrated Security=true;Trust Server Certificate = true ");
+
+connection.Open();
+using (connection)
+//{
+//    SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Employees", connection);
+//    int? employeesCount = (int?) await command.ExecuteScalarAsync();
+//    Console.WriteLine($"Count {employeesCount}");
+//}
+
+{
+    SqlCommand command = new SqlCommand("SELECT * FROM Employees", connection); 
+    //SqlDataReader reader = command.ExecuteReader();
+    SqlDataReader reader = await command.ExecuteReaderAsync();
+
+
+    using (reader)
+    {
+        while (reader.Read())
+        {
+            string name = reader["FirstName"].ToString().ToString();
+            string lastName = reader[2].ToString().ToString();
+            Console.WriteLine($"{name} {lastName}");
+        }
+    }
+}
