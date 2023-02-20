af8cf2962b222a01e9611b660c87b3633cf3b88b
Oleg Anokhin <oleg_anokhin@abv.bg>
2023-02-20 22:38:21 +0200

    Add new folder with solutions

---
+++ C#Entity Framework Core/02.ORM/MiniORM/ChangeTracker.cs
@@ -0,0 +1,14 @@
+﻿namespace MiniORM;
+
+    internal class ChangeTracker<T>
+    where T : class, new()
+    {
+        private readonly IList<T> allEntities;
+        private readonly IList<T> added;
+        private readonly IList<T> removed;
+public ChangeTracker(IEnumerable<T> allEntities)
+{
+    
+}
+
+    }
