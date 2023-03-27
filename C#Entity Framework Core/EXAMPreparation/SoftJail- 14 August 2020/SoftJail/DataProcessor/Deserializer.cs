72d6dcd106e9d8a8fabf42a1232b4baa9665bae4
Oleg Anokhin <oleg_anokhin@abv.bg>
2023-03-27 08:25:03 +0300

    Add new folders

---
+++ C#Entity Framework Core/EXAMPreparation/SoftJail- 14 August 2020/SoftJail/DataProcessor/Deserializer.cs
@@ -0,0 +1,34 @@
+﻿namespace SoftJail.DataProcessor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Data;
+
+    public class Deserializer
+    {
+        public static string ImportDepartmentsCells(SoftJailDbContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportPrisonersMails(SoftJailDbContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportOfficersPrisoners(SoftJailDbContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object obj)
+        {
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(obj);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResult, true);
+            return isValid;
+        }
+    }
+}
---
+++ C#Entity Framework Core/EXAMPreparation/SoftJail- 14 August 2020/SoftJail/DataProcessor/Serializer.cs
@@ -0,0 +1,18 @@
+﻿namespace SoftJail.DataProcessor
+{
+    using System;
+    using Data;
+
+    public class Serializer
+    {
+        public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
