a6a2df29b5201e9e1e6cc5bcfc0515fdc489f12c
Oleg Anokhin <oleg_anokhin@abv.bg>
2023-10-08 15:33:15 +0300

    Add solution for problem 02

--- Data Structures Fundamentals/01.Data Structures Linear - Lab/Problem02.Stack/Stack.cs
+++ Data Structures Fundamentals/01.Data Structures Linear - Lab/Problem02.Stack/Stack.cs
@@ -1,6 +1,7 @@
 ﻿namespace Problem02.Stack
 {
     using System;
+    using System.Data;
     using System.Collections;
     using System.Collections.Generic;
 
@@ -8,41 +9,83 @@
     {
         private class Node
         {
-            
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+            {
+                this.Element = element;
+            }
         }
 
         private Node top;
 
-        public int Count => throw new System.NotImplementedException();
+        public int Count { get; private set; }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            var node = new Node(item, this.top);
+            this.top = node;
+            this.Count++;
         }
 
         public T Pop()
         {
-            throw new System.NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var oldTop = this.top;
+            this.top = oldTop.Next;
+            this.Count--;
+            return oldTop.Element;
         }
 
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return this.top.Element;
         }
 
         public bool Contains(T item)
         {
-            throw new System.NotImplementedException();
+            var current = this.top;
+
+            while (current != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(current.Element, item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = this.top;
+
+            while (current != null)
+            {
+                {
+                    yield return current.Element;
+                    current = current.Next;
+                }
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+            => this.GetEnumerator();
     }
 }
